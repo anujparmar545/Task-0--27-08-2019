6b04dcdbe5f783c02438ccec52cd909a165bd310
Anuj Parmar <anujparmar545@gmail.com>
2019-08-27 09:23:27 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//First Commit
+		
+		Circle c1= new Circle();
+		double area=c1.getArea(5);
+		System.out.println("Area is= "+area);
+		
+		RectangleArea r=new RectangleArea();
+		area=r.getArea(5,3);
+		System.out.println("Area is= "+area);
+		
+	}
+
+}
